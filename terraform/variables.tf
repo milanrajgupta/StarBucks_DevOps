f4b90437a53a20cbe562acb05d83326986cce6dc
Milan Raz Gupta <66191482+milanrajgupta@users.noreply.github.com>
2026-07-22 12:49:14 +0530

    Fix formatting of variable types in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,11 +9,11 @@ variable "key_name" {
 
 variable "access_key" {
   default = "insert"                # aws access key
-  type = "string"
+  type = string
 }
 
 variable "secret_key" {
   default = "insert"         # aws secret key
-  type = "string"
+  type = string
 }
 
