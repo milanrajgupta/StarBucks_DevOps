971cf04c2a237d984ed656fba804e7bd9d503a52
Milan Raz Gupta <66191482+milanrajgupta@users.noreply.github.com>
2026-07-22 12:05:41 +0530

    Rename AWS key variable from 'starbucks1' to 'starbucks_key'

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,7 @@ variable "instance_name" {
 }
 
 variable "key_name" {
-  default = "starbucks1"                  # Names of key in aws
+  default = "starbucks_key"                  # Names of key in aws
 }
 
 
