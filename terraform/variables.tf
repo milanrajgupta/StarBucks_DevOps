09da1a123d2b261998ba5775fcdf9ad9af3f8d84
Milan Raz Gupta <66191482+milanrajgupta@users.noreply.github.com>
2026-07-22 12:57:17 +0530

    Refactor variable definitions in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,7 @@
 variable "instance_name" {
-    default = "Monitoring_server"  # Names of the instance
+  default = "Monitoring_server"
 }
 
 variable "key_name" {
-  default = "pathnext-ec2-key"                  # Names of key in aws
+  default = "pathnext-ec2-key"
 }
-
-
-
-
